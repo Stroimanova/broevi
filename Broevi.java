eb6b3de86cacef958775177b910f8067d5946f2d
unknown <simonastroimanova@hotmail.com>
2023-12-04 22:51:47 +0100

    first java program

---
+++ Broevi.java
@@ -0,0 +1,15 @@
+package proekt_66;
+
+public class Broevi {
+
+	public static void main(String[] args) {
+		int x=7;
+		int y=myFunc(x);
+		System.out.println(y);
+	}
+	public static int myFunc(int x) {
+		return x*7;
+	}
+}
+		
+
